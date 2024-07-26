6135504cadcf7e97941fe46cd208c380292adbd2
Christina <150205323+berryalien@users.noreply.github.com>
2024-07-26 13:49:42 +0400

    added logic for the accordion block in the js

--- js/main.js
+++ js/main.js
@@ -19,3 +19,9 @@ const swiperAbout = new Swiper(".about__slider", {
     prevEl: '.swiper-button-prev',
   },
     });
+
+document.querySelectorAll('.accordeon__triger').forEach((item) => {
+  item.addEventListener('click', () => {
+  item.parentNode.classList.toggle('accordeon__item--active')
+  })
+});
